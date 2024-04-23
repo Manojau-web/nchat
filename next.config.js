2627abd4007749d7b05a07e5308644d75ccc809c
Manojau-web <manojau2908@gmail.com>
2024-04-23 17:53:56 +0530

    new changes in package

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    swcPlugins: [["next-superjson-plugin", {}]]
+    //appDir: true,
+    //swcPlugins: [["next-superjson-plugin", {}]]
   },
   images: {
     domains: [
